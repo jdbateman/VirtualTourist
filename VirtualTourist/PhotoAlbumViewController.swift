57fc04d31afacbbadbad94b2df13fc90e61f91bd
jbateman <johnbateman00@gmail.com>
2015-09-19 14:22:59 -0700

    photo album

---
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by john bateman on 9/19/15.
+//  Copyright (c) 2015 John Bateman. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
